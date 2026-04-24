3377efcc22417039e8b29f08a564cde031fc9c12
sctilt <tiltonsc@gmail.com>
2026-04-23 21:20:22 -0400

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+import LiveScores from "./components/LiveScores";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Pressbox Pro</h1>
+      <LiveScores />
+    </main>
+  );
+}
