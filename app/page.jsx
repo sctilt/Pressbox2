6bae4fede5bc8da65a88aafd860311bd1c47890d
sctilt <tiltonsc@gmail.com>
2026-04-23 22:37:37 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,33 @@
-
+import DashboardLayout from "./components/DashboardLayout";
 import LiveScores from "./components/LiveScores";
+import PlayerChart from "./components/PlayerChart";
+import StatCard from "./components/StatCard";
+import { sampleChart } from "./data/sampleChart";
 
 export default function Page() {
   return (
-    <main>
-      <h1>Pressbox Pro</h1>
+    <DashboardLayout>
+
+      {/* Top stats row */}
+      <div style={{
+        display: "flex",
+        gap: 12,
+        marginBottom: 30
+      }}>
+        <StatCard label="Total Games" value="12" />
+        <StatCard label="Live Games" value="5" />
+        <StatCard label="Top Player" value="Ohtani" />
+      </div>
+
+      {/* Scores */}
       <LiveScores />
-    </main>
+
+      {/* Chart */}
+      <div style={{ marginTop: 40 }}>
+        <h2>Player Trend</h2>
+        <PlayerChart data={sampleChart} />
+      </div>
+
+    </DashboardLayout>
   );
 }
